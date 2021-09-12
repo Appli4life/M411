ede66241e05e8645f55d630aa024eccf2cf3174c
Simon Müller <simon.mueller@student.ipso.ch>
2021-09-12 21:28:40 +0200

    Test 1

---
+++ M411Solution/T1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Instanziieren
+                TrainingCalc tc = new TrainingCalc();
+
+                // Hinzufügen der Zeiten
+                tc.Add(14.41);
+                tc.Add(15.02);
+                tc.Add(15.11);
+                tc.Add(14.78);
+                tc.Add(14.89);
+                tc.Add(15.05);
+
+                // Schreiben der Resultate
+                Console.WriteLine($"Durchschnitt: {tc.GetAvg()}");
+                Console.WriteLine($"Summe: {tc.GetSum()}");
+                Console.WriteLine($"Min: {tc.GetMin()}");
+                Console.WriteLine($"Max: {tc.GetMax()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ M411Solution/T1/TrainingCalc.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace T1
+{
+    /// <summary>
+    /// Klasse TrainingCalc
+    /// </summary>
+    public class TrainingCalc
+    {
+        /// <summary>
+        /// Rundenzeiten
+        /// </summary>
+        protected double[] Rundenzeiten; // Theoretisch mit einer Liste eifacher (Ich wollte eine Herausforderung :))
+
+        /// <summary>
+        /// Zähler
+        /// </summary>
+        public int Count { get; set; } = 0;
+
+        /// <summary>
+        /// Hinzufügen
+        /// </summary>
+        /// <param name="zeit">Rundenzeit</param>
+        public void Add(double zeit)
+        {
+            // Der lauf kann nicht in 0 Sekunden erfolgen!
+            if (zeit < 0.0)
+            {
+                throw new Exception($"Ungültige Rundenzeit. Rundenzeit: {zeit}");
+            }
+            // Beim Hinzufügen wird das Array um eines Verlängert
+            double[] temp = new double[Count + 1];
+            Array.Copy(Rundenzeiten, temp, this.Count);
+            Rundenzeiten = temp;
+            Rundenzeiten[Count] = zeit;
+            Count++;
+        }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public TrainingCalc()
+        {
+            Rundenzeiten = new double[0];
+        }
+
+        /// <summary>
+        /// Nidrigste Rundenzeiz
+        /// </summary>
+        /// <returns>Rundenzeit</returns>
+        public double GetMin()
+        {
+            double m = Rundenzeiten[0];
+            foreach (var item in Rundenzeiten)
+            {
+                if (item < m)
+                {
+                    m = item;
+                }
+            }
+            return m;
+        }
+
+        /// <summary>
+        /// Höchste Rundenzeit
+        /// </summary>
+        /// <returns>Rundenzeit</returns>
+        public double GetMax()
+        {
+            double m = Rundenzeiten[0];
+            foreach (var item in Rundenzeiten)
+            {
+                if (item > m)
+                {
+                    m = item;
+                }
+            }
+            return m;
+        }
+
+        /// <summary>
+        /// Durchschnitt
+        /// </summary>
+        /// <returns>Rundenzeit Durchschnitt</returns>
+        public double GetAvg()
+        {
+            // Durch null teilen geht nicht
+            if (Count == 0)
+            {
+                throw new Exception($"Durchschnitt kann nicht mit keinen Zahlen gerechnet werden!");
+            }
+
+            double d = 0;
+            foreach (var item in Rundenzeiten)
+            {
+                d += item;
+            }
+
+            d = d / this.Count;
+
+            return d;
+        }
+
+        /// <summary>
+        /// Summe der Rundenzahlen
+        /// </summary>
+        /// <returns></returns>
+        public double GetSum()
+        {
+            double s = 0;
+            foreach (var item in Rundenzeiten)
+            {
+                s += item;
+            }
+            return s;
+        }
+    }
+}
